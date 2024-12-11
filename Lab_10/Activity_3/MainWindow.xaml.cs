bb10c54b61c99dba99d30cc1eee112c00d499a6c
syedafatima-cloud <syedafatimafaisal07@gmail.com>
2024-12-11 14:21:45 +0500

    Add files via upload

---
+++ Lab_10/Activity_3/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace ComboBoxExample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Event handler for ComboBox selection change
+        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            // Get the selected ComboBoxItem
+            var selectedItem = comboBox.SelectedItem as System.Windows.Controls.ComboBoxItem;
+
+            // If an item is selected, update the TextBox with its content
+            if (selectedItem != null)
+            {
+                textBox.Text = selectedItem.Content.ToString();
+            }
+        }
+    }
+}
