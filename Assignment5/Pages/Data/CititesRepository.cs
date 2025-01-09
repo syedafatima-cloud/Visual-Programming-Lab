87ecfb546627c7f2151bc9f9ac0627839bb6aa93
syedafatima-cloud <syedafatimafaisal07@gmail.com>
2025-01-09 16:10:28 +0500

    Add files via upload

---
+++ Assignment5/Pages/Data/CititesRepository.cs
@@ -0,0 +1,17 @@
+﻿// File: CitiesRepository.cs
+using System.Collections.Generic;
+
+public static class CitiesRepository
+{
+    public static List<string> GetCities()
+    {
+        return new List<string>
+        {
+            "Lahore",
+            "Karachi",
+            "Islamabad",
+            "Peshawar",
+            "Quetta"
+        };
+    }
+}
